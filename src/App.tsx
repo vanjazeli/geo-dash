8915ea6a2172076a6cd8109fc70bc5ccd61446e0
vanjazeli <vanjazeli@gmail.com>
2023-09-28 10:01:13 +0200

    Feat: Add main data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { RootState } from "./store/store";
-
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { setLoadingOff } from "./store/slices/loadingSlice";
 
 import Loader from "./components/ui/Loader";
 
+import fetchData from "./services/fetchData";
+
+import { Quiz } from "./types/QuizType";
+import { RootState } from "./store/store";
+
 function App() {
 	const isLoading = useSelector((state: RootState) => state.loading.value);
 	const dispatch = useDispatch();
 
-	console.log(isLoading);
+	const [quizes, setQuizes] = useState<Quiz[] | null>(null);
+
+	useEffect(() => {
+		fetchData('http://localhost:8001/quizes', (data) => {
+			setQuizes(data);
+			dispatch(setLoadingOff());
+		})
+	}, []);
 
   return (
     <>
