c24a7e7df795e8b99fd2fe6015987152d4b943a8
vanjazeli <vanjazeli@gmail.com>
2023-10-05 14:55:53 +0200

    Fix: Remove FlagQuizPage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,34 @@
 import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setLoadingOff, setLoadingOn } from './store/slices/loadingSlice';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Loader from './components/ui/Loader';
+import PageWrap from './components/ui/PageWrap';
 import IndexPage from './components/pages/IndexPage';
-import FlagQuizPage from './components/pages/FlagQuizPage';
+import FlagQuiz from './components/containers/FlagQuiz';
 
 import fetchData from './services/fetchData';
 
 import { Quiz } from './types/QuizType';
-import { RootState } from './store/store';
 
 function App() {
-	const isLoading = useSelector((state: RootState) => state.loading.value);
-	const dispatch = useDispatch();
-
 	const [quizes, setQuizes] = useState<Quiz[] | null>(null);
 
 	useEffect(() => {
-		dispatch(setLoadingOn());
 		fetchData('http://localhost:8001/quizes', (data) => {
 			setQuizes(data);
-			dispatch(setLoadingOff());
 		});
 	}, []);
 
 	return (
-		<>
-			{isLoading && <Loader />}
+		<PageWrap>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<IndexPage quizes={quizes} />} />
 					{quizes?.map((quiz, quizIndex) => (
-						<Route path={`/${quiz.name}`} element={<FlagQuizPage quiz={quiz} />} key={quizIndex} />
+						<Route path={`/${quiz.name}`} element={<FlagQuiz sentence="Which country's flag is showing below?" quiz={quiz} />} key={quizIndex} />
 					))}
 				</Routes>
 			</BrowserRouter>
-		</>
+		</PageWrap>
 	);
 }
 
